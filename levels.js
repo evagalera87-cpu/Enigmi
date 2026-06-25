492e52d01193fddc4ed22721107b24a0fbb95192
evagalera87-cpu <evagalera87@gmail.com>
2026-06-25 11:37:30 +0200

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,28 @@
+<script src="levels.js"></script>
+<script>
+  let level = Number(localStorage.getItem("level")) || 0;
+  ...
+</script>
+
+const levels = [
+  {
+    q: "2 + 2",
+    a: "4"
+  },
+  {
+    q: "Capitale d'Italia",
+    a: "roma"
+  },
+  {
+    q: "Sono pieno di buchi ma trattengo l'acqua",
+    a: "spugna"
+  },
+  {
+    q: "Gira ma non si muove mai da casa",
+    a: "ruota"
+  },
+  {
+    q: "Più ne togli, più divento grande",
+    a: "buco"
+  }
+];
